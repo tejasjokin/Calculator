0f0423db91e267ac23d3cac64fd78de257a84c57
Tejas Machkar <tejass.machkar@gmail.com>
2022-08-17 17:46:01 +0530

    Added Add and Subtract functions in CalculatorFunctions class

--- Calculator/CalculatorFunctions.cs
+++ Calculator/CalculatorFunctions.cs
@@ -8,5 +8,14 @@ namespace Calculator
 {
     public class CalculatorFunctions
     {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtract(int a, int b)
+        {
+            return a- b;
+        }
     }
 }
